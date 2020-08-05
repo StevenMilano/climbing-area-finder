e3a5f0c6fb79b27815380ebad6268c20d67c2787
Steven Milano <milanosteven@gmail.com>
2020-08-05 17:41:41 -0400

    Added Mountain project api & linked it with Google api

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var map, infoWindow;
+var lon;
+var lat;
+let mountainProjectURL = "https://www.mountainproject.com/data/get-routes-for-lat-lon";
+let mountainProjectKey = "200793847-4c8ca95c19e7a222488e00479926bf74"
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -20,50 +24,61 @@ function geocodeAddress(geocoder, resultsMap) {
         map: resultsMap,
         position: results[0].geometry.location
       });
+      console.log(results[0].geometry.location.lng());
+      console.log(results[0].geometry.location.lat());
+      lon = results[0].geometry.location.lng();
+      lat = results[0].geometry.location.lat();
+      getLngLat(lat, lon);
     } else {
       alert("Geocode was not successful for the following reason: " + status);
     }
   });
 }
 
-function  watchForm () {};
+function formatQueryParams(params) {
+  var qureyItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+      return qureyItems.join('&');
+}
 
-function getLngLat () {};
+function getLngLat (querry) {
+  var params = {
+    key: mountainProjectKey,
+    lat: lat,
+    lon: lon,
+    // maxDistance: querry,
+    // maxResults: querry
+  };
+  var queryString = formatQueryParams(params);
+  var url = mountainProjectURL+ '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(respone => {
+      if(respone.ok) {
+        return respone.json();
+      }
+      throw new Error(respons.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong ${err.message}`);
+    });
+};
 
 function getResults () {};
 
 function getMarkers () {};
 
-function displayResults () {};
-
-
-      //   infoWindow = new google.maps.InfoWindow;
-
-      //   // Try HTML5 geolocation.
-      //   if (navigator.geolocation) {
-      //     navigator.geolocation.getCurrentPosition(function(position) {
-      //       var pos = {
-      //         lat: position.coords.latitude,
-      //         lng: position.coords.longitude
-      //       };
-
-      //       infoWindow.setPosition(pos);
-      //       infoWindow.setContent('Location found.');
-      //       infoWindow.open(map);
-      //       map.setCenter(pos);
-      //     }, function() {
-      //       handleLocationError(true, infoWindow, map.getCenter());
-      //     });
-      //   } else {
-      //     // Browser doesn't support Geolocation
-      //     handleLocationError(false, infoWindow, map.getCenter());
-      //   }
-      // }
-
-      // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      //   infoWindow.setPosition(pos);
-      //   infoWindow.setContent(browserHasGeolocation ?
-      //                         'Error: The Geolocation service failed.' :
-      //                         'Error: Your browser doesn\'t support geolocation.');
-      //   infoWindow.open(map);
-      // }
+function displayResults () {
+  console.log(responseJson);
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.data.length; i++) {
+      $('#results-list').append(
+          `<li><a href='${responseJson.data[i].url}'><h3>${responseJson.data[i].fullName}</h3></a>
+          <p>${responseJson.data[i].description}</p>
+          </li>`
+        )};
+  $('#search-results').removeClass('hidden');
+}
