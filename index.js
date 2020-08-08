3cb40601e8fdbe75b9f624a9c39aa89a4fb1d893
Steven Milano <milanosteven@gmail.com>
2020-08-08 09:36:05 -0400

    Added comments

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var maxDistance;
 let mountainProjectURL = "https://www.mountainproject.com/data/get-routes-for-lat-lon";
 let mountainProjectKey = "200793847-4c8ca95c19e7a222488e00479926bf74"
 
+//intilizes the map to the webpage.
+//map variable is set here and needs to be passed all the way down in order for markers to be placed on the map.
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
@@ -17,6 +19,8 @@ function initMap() {
   });
 }
 
+//Grabs address from form and gets the lat and lng.
+//map is set to results map and needs to be passed as an argument for getLngLat
 function geocodeAddress(geocoder, resultsMap) {
   const address = document.getElementById("address").value;
   geocoder.geocode({ address: address }, (results, status) => {
@@ -34,6 +38,7 @@ function geocodeAddress(geocoder, resultsMap) {
   });
 }
 
+//functin for getting data from various sources and placing them into variables to be used throughout. 
 function getResults(results) {
   lon = results[0].geometry.location.lng();
   lat = results[0].geometry.location.lat();
@@ -41,12 +46,14 @@ function getResults(results) {
   maxDistance = $('#distance').val();
 }
 
+//formats the url used to access the mountain project API
 function formatQueryParams(params) {
   var qureyItems = Object.keys(params)
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
       return qureyItems.join('&');
 }
 
+//This access the Mountain project api and runs the functions to place markers on the map as well as display the results below the map.
 function getLngLat (querry1, querry2, querry3, limit=50, map) {
   var params = {
     key: mountainProjectKey,
@@ -66,6 +73,7 @@ function getLngLat (querry1, querry2, querry3, limit=50, map) {
       }
       throw new Error(respons.statusText);
     })
+    //setMarkers needs to be run before displayResults.
     .then((responseJson) => {
       setMarkers (map, responseJson);
       displayResults(responseJson);
@@ -75,6 +83,8 @@ function getLngLat (querry1, querry2, querry3, limit=50, map) {
     });
 };
 
+
+//sets the markers onto the map with the lat and lng from the mountain project Json object
 function setMarkers (map, responseJson) {
   for (let j = 0; j < responseJson.routes.length; j++) {
     var marker = new google.maps.Marker({
@@ -86,6 +96,7 @@ function setMarkers (map, responseJson) {
   marker.setMap(map);
 };
 
+//handles displaying the results from the Json object
 function displayResults (responseJson) {
   $('#search-results').removeClass('hidden');
   $('#results-list').empty();
